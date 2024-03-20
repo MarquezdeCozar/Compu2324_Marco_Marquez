b35a6bc3929162ac58fc510e5863bd57ee419aab
Marco Andrés Márquez <marcomarquezdecozar@gmail.com>
2024-03-20 17:41:54 +0100

    na

--- suma_vec.c
+++ suma_vec.c
@@ -1,8 +1,7 @@
 // Programa que suma dos vectores que entren de punteros
 
-void suma_vec(double *a, double *b, int dim) {
-    int i;
-    for (i = 0; i < dim; i++) {
-        *(a + i) += *(b + i);
+void suma_vectores(double *a, double *b, int dim) {
+    for (int i = 0; i < dim; i++) {
+        a[i] += b[i];
     }
 }
