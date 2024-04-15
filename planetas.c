4a7a9ee65ff0caa47abbc76760cd00bb4fa13841
Marco Andrés Márquez <marcomarquezdecozar@gmail.com>
2024-04-15 19:20:35 +0200

    programa planetas que funciona con los 4 primeros

--- planetas.c
+++ planetas.c
@@ -4,19 +4,21 @@
 
 #define G 6.67430e-11
 #define c 1.496e11
+#define h 86400
+#define t 365 * 24 * 3600
 
 double distancia(double x1, double y1, double x2, double y2) 
 {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
-void aceleracion(double *ax, double *ay, double ax_0, double ay_0, double x, double y, double m, double *x_pos, double *y_pos, double *masa, int n_planetas) 
+void aceleracion(double *ax, double *ay, double x, double y, double m, double *wx, double *wy, double *x_pos, double *y_pos, double *masa, int n_planetas) 
 {
     
     double dist, f;
 
-    ax_0 = *ax;
-    ay_0 = *ay;
+    *wx = *ax * h * 0.5;
+    *wy = *ay * h * 0.5;
 
     *ax = 0;
     *ay = 0;
@@ -26,36 +28,49 @@ void aceleracion(double *ax, double *ay, double ax_0, double ay_0, double x, dou
         if (m != masa[i]) 
         {
             dist = distancia(x, y, x_pos[i], y_pos[i]);
-            f = G * m * masa[i] / pow(dist, 3);
+            f = G  * masa[i] / pow(dist, 3);
             
-            *ax += f * (x_pos[i]-x) / m;
-            *ay += f * (y_pos[i]-y) / m;
+            *ax += f * (x_pos[i]-x) ;
+            *ay += f * (y_pos[i]-y) ;
         }
     }
 }
 
-// Algoritmo de Verlet
-void verlet(double *x, double *y, double *vx, double *vy, double ax, double ay, double ax_0, double ay_0, double h) 
+
+void pos(double *x, double *y, double *vx, double *vy, double *wx, double *wy, double *ax, double *ay)
 {
+    *x += *vx * h + 0.5 * *ax * h * h;
+    *y += *vy * h + 0.5 * *ay * h * h;
 
-    *x += *vx * h + 0.5 * ax * h * h;
-    *y += *vy * h + 0.5 * ay * h * h;
-    *vx += (ax_0 + ax) * h * 0.5;
-    *vy += (ay_0 + ay) * h * 0.5;
+    *wx += *vx + 0.5 * h * *ax;
+    *wy += *vy + 0.5 * h * *ay;
 
+}
+
+
+void veloc(double *x, double *y, double *vx, double *vy, double *wx, double *wy, double *ax, double *ay) 
+{
+    *vx += *wx + *ax * h * 0.5;
+    *vy += *wy + *ay * h * 0.5;
 }
 
 int main() 
 {
     FILE *salida = fopen("posiciones.txt", "w");
 
-    int n_planetas = 2; // Número de planetas usados
+    int n_planetas = 5; // Número de planetas usados
+
 
     double *x = malloc(n_planetas * sizeof(double));
     double *y = malloc(n_planetas * sizeof(double));
     double *vx = malloc(n_planetas * sizeof(double));
     double *vy = malloc(n_planetas * sizeof(double));
+    double *wx = malloc(n_planetas * sizeof(double));
+    double *wy = malloc(n_planetas * sizeof(double));
     double *m = malloc(n_planetas * sizeof(double));
+    double *ax = malloc(n_planetas * sizeof(double));
+    double *ay = malloc(n_planetas * sizeof(double));
+    
 
     // Datos de los planetas
     x[0] = 0;
@@ -88,30 +103,30 @@ int main()
     vy[4] = 24130;
     m[4] = 6.421e23;
 
-    double h = 86400; // Unidad de paso
-    double t = 365 * 24 * 3600; // Tiempo total
+    for(int i=0; i < n_planetas; i++)
+    {
+        aceleracion(&ax[i], &ay[i], x[i], y[i], m[i], &wx[i], &wy[i], x, y, m, n_planetas);
+    }
 
     for (double paso = 0; paso < t; paso += h) 
     {
         for (int i = 0; i < n_planetas; i++) 
         {
-            x[i] =x[i]/c;
-            y[i]=y[i]/c;
+            x[i] = x[i]/c;
+            y[i] = y[i]/c;
             fprintf(salida, "%lf, %lf ", x[i], y[i]);
             fprintf(salida, "\n");
-            x[i]=x[i]*c;
-            y[i]=y[i]*c;
+            x[i] = x[i]*c;
+            y[i] = y[i]*c;
         }
         fprintf(salida, "\n");
 
         // Actualizar aceleraciones
         for (int i = 0; i < n_planetas; i++) 
         {
-            double ax, ay, ax_0, ay_0;
-            ax = 0;
-            ay = 0;
-            aceleracion(&ax, &ay, ax_0, ay_0, x[i], y[i], m[i], x, y, m, n_planetas);
-            verlet(&x[i], &y[i], &vx[i], &vy[i], ax, ay, ax_0, ay_0, h);
+            pos(&x[i], &y[i], &vx[i], &vy[i], &wx[i], &wy[i], &ax[i], &ay[i]);
+            aceleracion(&ax[i], &ay[i], x[i], y[i], m[i], &wx[i], &wy[i], x, y, m, n_planetas);
+            veloc(&x[i], &y[i], &vx[i], &vy[i], &wx[i], &wy[i], &ax[i], &ay[i]);
         }
     }
 
@@ -121,6 +136,8 @@ int main()
     free(y);
     free(vx);
     free(vy);
+    free(wx);
+    free(wy);
     free(m);
 
     return 0;
